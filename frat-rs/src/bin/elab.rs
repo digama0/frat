f50b50ed7d1d69b21b032f295cb14c6fb22d3871
Mario Carneiro <di.gama@gmail.com>
2019-11-13 23:19:55 -0500

    move elab to separate binary of frat-rs crate

--- frat-rs/src/bin/elab.rs
+++ frat-rs/src/bin/elab.rs
@@ -1,4 +1,4 @@
-mod parser;
+#[path="../parser.rs"] mod parser;
 
 // use std::process::exit;
 // use std::collections::HashMap;
@@ -45,7 +45,7 @@ pub enum ElabStep {
   Del(u64),
 }
 
-// Type used to represent the status of a clause under a specific variable assigntment. 
+// Type used to represent the status of a clause under a specific variable assigntment.
 pub enum ClaSta {
   Unsat,
   Unit(i64),
@@ -56,9 +56,9 @@ pub enum ClaSta {
 // #[derive(Debug)]
 struct IdCla<'a> {
   id: u64,
-  marked: bool, 
+  marked: bool,
   used: bool,
-  cla: & 'a Clause 
+  cla: & 'a Clause
 }
 
 struct BackParser {
@@ -204,16 +204,16 @@ fn clause_status(v: &Vec<i64>, c: &Clause) -> ClaSta {
     }
   }
 
-  match uf { 
+  match uf {
     None => ClaSta::Unsat,
     Some(l) => ClaSta::Unit(l)
   }
 }
 
-// Derive a new subunit clause and update all arguments accordingly. 
-// Return true if a contradiction was found, false if a unit clause 
+// Derive a new subunit clause and update all arguments accordingly.
+// Return true if a contradiction was found, false if a unit clause
 // was derived, and panic if neither
-fn propagate_once(is: &mut Vec<u64>, v: &mut Vec<i64>, ics: &mut Vec<IdCla>) -> bool { 
+fn propagate_once(is: &mut Vec<u64>, v: &mut Vec<i64>, ics: &mut Vec<IdCla>) -> bool {
   for ic in ics {
     if !ic.used { // Only consider clauses that have not been used for UP yet
       match clause_status(v, ic.cla) {
@@ -222,12 +222,12 @@ fn propagate_once(is: &mut Vec<u64>, v: &mut Vec<i64>, ics: &mut Vec<IdCla>) -> 
           ic.used = true;
           return true;
         }
-        ClaSta::Unit(l) => { 
+        ClaSta::Unit(l) => {
           is.push(ic.id);
           v.push(l);
           ic.used = true;
           return false;
-        } 
+        }
         ClaSta:: Plural => ()
       }
     }
@@ -236,12 +236,12 @@ fn propagate_once(is: &mut Vec<u64>, v: &mut Vec<i64>, ics: &mut Vec<IdCla>) -> 
   panic!("Unit progress stuck");
 }
 
-fn propagate_core(mut is: Vec<u64>, mut v: Vec<i64>, mut cs: Vec<(IdCla)>) -> Vec<u64> { 
-  if propagate_once(&mut is, &mut v, &mut cs) { is } 
+fn propagate_core(mut is: Vec<u64>, mut v: Vec<i64>, mut cs: Vec<(IdCla)>) -> Vec<u64> {
+  if propagate_once(&mut is, &mut v, &mut cs) { is }
   else { propagate_core(is, v, cs) }
 }
 
-fn propagate(v: Vec<i64>, cs: Vec<IdCla>) -> Vec<u64> { 
+fn propagate(v: Vec<i64>, cs: Vec<IdCla>) -> Vec<u64> {
   let is: Vec<u64> = Vec::new();
   propagate_core(is, v, cs)
 }
@@ -256,17 +256,17 @@ fn undelete(is: &Vec<u64>, cs: &mut HashMap<u64, (bool, Clause)>, temp: &mut Fil
   }
 }
 
-trait Binary { 
+trait Binary {
   fn encode(x: &Self) -> Vec<u8>;
   // fn decode(v: Vec<u8>) -> Option<Vec<u8>>;
 }
 
-impl Binary for u64 { 
-  fn encode(x: &u64) -> Vec<u8> { 
-    
+impl Binary for u64 {
+  fn encode(x: &u64) -> Vec<u8> {
+
     let mut y = *x;
     let mut v = Vec::new();
-    
+
     while 128 <= y {
       let u = (y % 128) as u8;
       v.push(u);
@@ -277,25 +277,25 @@ impl Binary for u64 {
   }
 }
 
-impl Binary for i64 { 
-  fn encode(x: &i64) -> Vec<u8> { 
-    if *x < 0 { 
+impl Binary for i64 {
+  fn encode(x: &i64) -> Vec<u8> {
+    if *x < 0 {
       return Binary::encode(&(((2 * -x) + 1) as u64))
-    } else { 
+    } else {
       return Binary::encode(&((2 * x) as u64))
     }
   }
 }
 
 fn encode_vector<T: Binary>(ts: &Vec<T>) -> Vec<u8> {
-  let mut v = Vec::new(); 
+  let mut v = Vec::new();
   for t in ts {v.append(&mut Binary::encode(t));}
   v
 }
 
-impl Binary for ElabStep { 
-  fn encode(x: &ElabStep) -> Vec<u8> { 
-    match x { 
+impl Binary for ElabStep {
+  fn encode(x: &ElabStep) -> Vec<u8> {
+    match x {
       ElabStep::Orig(i, ls) => {
         let mut v = vec!['o' as u8];
         v.append(&mut Binary::encode(i));
@@ -328,7 +328,7 @@ fn elab(frat: File, temp: &mut File) -> io::Result<()> {
   let mut cs: HashMap<u64, (bool, Clause)> = HashMap::new();
 
   while let Some(s) = bp.next() {
-    
+
     match s {
 
       Step::Orig(i, ls) => {
@@ -340,14 +340,14 @@ fn elab(frat: File, temp: &mut File) -> io::Result<()> {
       }
 
       Step::Add(i, ls, p) => {
-        if cs.get(&i).unwrap().0 { 
+        if cs.get(&i).unwrap().0 {
           let is : Vec<u64> = match p {
             None => {
               // v is the valuation obtained by negating all literals in the clause to be added
               let v : Vec<i64> = ls.iter().map(|x| -x).collect();
-              // scs is all the potentially vailable clauses (i.e., both active and passive) 
+              // scs is all the potentially vailable clauses (i.e., both active and passive)
               // against which v will be shown to be unsat
-              
+
               let ics : Vec<IdCla> = cs.iter().map(|x| IdCla {id: *x.0, marked:(x.1).0, used: false, cla: &(x.1).1}).collect();
               // js is the IDs of clauses used in showing v unsat
               propagate(v, ics)
@@ -357,17 +357,17 @@ fn elab(frat: File, temp: &mut File) -> io::Result<()> {
           undelete(&is, &mut cs, temp);
           temp.write(&Binary::encode(&ElabStep::Add(i, ls, is)))
             .expect("Failed to write add step");
-        } 
+        }
 
         cs.remove(&i);
       },
       Step::Del(i, ls) => {
-        assert_eq!(None, cs.insert(i, (false, ls)), 
+        assert_eq!(None, cs.insert(i, (false, ls)),
           "Encountered a delete step for preexisting clause");
       },
       Step::Final(i, ls) => {
         // Identical to the Del case, except that the clause should be marked if empty
-        assert_eq!(None, cs.insert(i, (ls.is_empty(), ls)), 
+        assert_eq!(None, cs.insert(i, (ls.is_empty(), ls)),
           "Encountered a delete step for preexisting clause");
       }
       Step::Todo(_i) => ()
@@ -381,10 +381,10 @@ fn trim_bin(cnf: Vec<parser::Clause>, temp: File, lrat: &mut File) -> io::Result
   let mut k: u64 = cnf.len().try_into().unwrap(); // Counter for the last used ID
   let mut m: HashMap<u64, u64> = HashMap::new(); // Mapping between old and new IDs
   let mut bp = BackParser::new(temp)?.peekable();
-  
+
   while let Some(s) = bp.next() {
 
-    match s { 
+    match s {
 
       Step::Orig(i, ls) => {
         assert!(!m.contains_key(&i), "Multiple orig steps with duplicate IDs");
@@ -429,17 +429,17 @@ fn trim_bin(cnf: Vec<parser::Clause>, temp: File, lrat: &mut File) -> io::Result
 // fn calc_lrat(p: Vec<parser::Clause>, ls: Vec<Lstep>) -> Vec<Lstep> {
 //   let k: u64 = p.len().try_into().unwrap();
 //   let mut m: Vec<(u64, u64)> = Vec::new();
-//   calc_id_map(p, k, &ls, &mut m); 
-//    
+//   calc_id_map(p, k, &ls, &mut m);
+//
 //   calc_lrat_core(m, ls)
 // }
 
 fn is_perm(v: &Vec<i64>, w: &Vec<i64>) -> bool {
   if v.len() != w.len() { return false }
-	for i in v {
-	  if !w.contains(i) { return false }
+  for i in v {
+    if !w.contains(i) { return false }
   }
-	true
+  true
 }
 
 // fn calc_id_map(p: Vec<parser::Clause>, mut k: u64, lns: &Vec<Lstep>, m: &mut Vec<(u64, u64)>) {
@@ -461,23 +461,23 @@ fn is_perm(v: &Vec<i64>, w: &Vec<i64>) -> bool {
 //   }
 // }
 
-// fn map_id(m: &Vec<(u64, u64)>, i: &u64) -> u64 { 
-//   let j = m.iter().position(|x| x.0 == *i).unwrap(); 
+// fn map_id(m: &Vec<(u64, u64)>, i: &u64) -> u64 {
+//   let j = m.iter().position(|x| x.0 == *i).unwrap();
 //   m[j].1
 // }
-// 
-// fn calc_lrat_core(m: Vec<(u64, u64)>, ls: Vec<Lstep>) -> Vec<Lstep> { 
+//
+// fn calc_lrat_core(m: Vec<(u64, u64)>, ls: Vec<Lstep>) -> Vec<Lstep> {
 //   let mut lr: Vec<Lstep> = Vec::new();
-// 
-//   for l in ls { 
+//
+//   for l in ls {
 //     match l {
 //       Lstep::Add(i, ls, is) => {
-//         let new_i = map_id(&m, &i); 
+//         let new_i = map_id(&m, &i);
 //         let new_is: Vec<u64> = is.iter().map(|x| map_id(&m, &x)).collect();
 //         if ls.is_empty() {
 //           lr.push(Lstep::Add(new_i, ls, new_is));
 //           return lr;
-//         } else { 
+//         } else {
 //           lr.push(Lstep::Add(new_i, ls, new_is));
 //         }
 //       }
@@ -487,10 +487,10 @@ fn is_perm(v: &Vec<i64>, w: &Vec<i64>) -> bool {
 //       Lstep::Orig(_, _) => ()
 //     }
 //   }
-// 
+//
 //   lr
 // }
- 
+
 fn main() -> io::Result<()> {
   let mut args = env::args().skip(1);
   let dimacs = args.next().expect("missing input file");
@@ -506,5 +506,5 @@ fn main() -> io::Result<()> {
   let (_vars, cnf) = parse_dimacs(read_to_string(dimacs)?.chars());
   trim_bin(cnf, temp_read, &mut lrat)?;
 
-	Ok(())
+  Ok(())
 }
