1253a71029b418038330a8d5fb8fce2dfda8530b
Mario Carneiro <di.gama@gmail.com>
2019-11-13 22:19:52 -0500

    add serializer

--- frat-rs/src/lratchk.rs
+++ frat-rs/src/lratchk.rs
@@ -1,7 +1,8 @@
 use std::fs::File;
 use std::process::exit;
 use std::collections::HashMap;
-use std::io::{self, Read, Seek, SeekFrom};
+use std::io::{self, Read, Seek, SeekFrom, Write};
+use arrayvec::ArrayVec;
 use super::parser::{parse_unum, parse_num};
 
 const BUFFER_SIZE: usize = 0x4000;
@@ -162,12 +163,70 @@ impl Iterator for BackParser {
   }
 }
 
-fn subsumes(clause: &Clause, clause2: &Clause) -> bool {
-  clause2.iter().all(|lit2| clause.contains(lit2))
+trait Serialize {
+  fn write<W: Write>(&self, w: &mut W) -> io::Result<()>;
 }
 
-fn print_seg(seg: &Segment) {
-  unimplemented!()
+impl<A: Serialize, B: Serialize> Serialize for (A, B) {
+  fn write<W: Write>(&self, w: &mut W) -> io::Result<()> { self.0.write(w)?; self.1.write(w) }
+}
+
+impl Serialize for u8 {
+  fn write<W: Write>(&self, w: &mut W) -> io::Result<()> { w.write_all(&[*self]) }
+}
+
+impl Serialize for char {
+  fn write<W: Write>(&self, w: &mut W) -> io::Result<()> { (*self as u8).write(w) }
+}
+
+impl<'a, A: Serialize> Serialize for &'a Vec<A> {
+  fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
+    for v in *self { v.write(w)? }
+    0u8.write(w)
+  }
+}
+
+impl Serialize for u64 {
+  fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
+    let mut val = *self;
+    let mut buf = ArrayVec::<[u8; 10]>::new();
+    loop {
+      if val & !0x7f == 0 {
+        buf.push((val & 0x7f) as u8);
+        return w.write_all(&buf)
+      } else {
+        buf.push((val | 0x80) as u8);
+        val >>= 7;
+      }
+    }
+  }
+}
+
+impl Serialize for i64 {
+  fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
+      let u: u64 = if *self < 0 { -*self as u64 * 2 + 1 } else { *self as u64 * 2 };
+      u.write(w)
+  }
+}
+
+impl Serialize for Step {
+  fn write<W: Write>(&self, w: &mut W) -> io::Result<()> {
+    match *self {
+      Step::Orig(idx, ref vec) => ('o', (idx, vec)).write(w),
+      Step::Add(idx, ref vec, None) => ('a', (idx, vec)).write(w),
+      Step::Add(idx, ref vec, Some(Proof::Sorry)) =>
+        (('t', (999u8, 0u8)), ('a', (idx, vec))).write(w),
+      Step::Add(idx, ref vec, Some(Proof::LRAT(ref steps))) =>
+        (('a', (idx, vec)), ('l', steps)).write(w),
+      Step::Del(idx, ref vec) => ('d', (idx, vec)).write(w),
+      Step::Final(idx, ref vec) => ('f', (idx, vec)).write(w),
+      Step::Todo(idx) => ('t', (idx, 0u8)).write(w),
+    }
+  }
+}
+
+fn subsumes(clause: &Clause, clause2: &Clause) -> bool {
+  clause2.iter().all(|lit2| clause.contains(lit2))
 }
 
 fn check_proof_step(_active: &mut HashMap<u64, (bool, Clause)>, _cl: &Clause, p: Option<Proof>) -> Option<Proof> {
@@ -207,36 +266,30 @@ pub fn check_proof(proof: File) -> io::Result<()> {
           *todos.entry(0).or_insert(0i64) += 1;
           // eprintln!("added clause {} {:?} has no proof and no todo", i, lits);
         }
-        match active.remove(&i) {
-          None => {
-            dirty_add += 1;
-            // eprintln!("added clause {} {:?} never finalized", i, lits);
-          },
-          Some((need, lits2)) => {
-            if !subsumes(&lits2, &lits) {
-              eprintln!("added {:?}, removed {:?}", lits2, lits);
-              bad = true;
-            }
-            if need {
-              match check_proof_step(&mut active, &lits, p) {
-                None => {
-                  eprintln!("bad proof for {:?}", lits);
-                  bad = true;
-                },
-                Some(p) => {
-                  if let Proof::LRAT(steps) = p {
-                    for s in steps {
-                      let needed = &mut active.get_mut(&s).expect("bad LRAT proof").0;
-                      if !*needed {
-                        unimplemented!();
-                        *needed = true;
-                      }
-                    }
+        if let Some((need, lits2)) = active.remove(&i) {
+          if !subsumes(&lits2, &lits) {
+            eprintln!("added {:?}, removed {:?}", lits2, lits);
+            bad = true;
+          }
+          if need {
+            if let Some(p) = check_proof_step(&mut active, &lits, p) {
+              if let Proof::LRAT(steps) = p {
+                for s in steps {
+                  let needed = &mut active.get_mut(&s).expect("bad LRAT proof").0;
+                  if !*needed {
+                    unimplemented!();
+                    *needed = true;
                   }
                 }
               }
+            } else {
+              eprintln!("bad proof for {:?}", lits);
+              bad = true;
             }
           }
+        } else {
+          dirty_add += 1;
+          // eprintln!("added clause {} {:?} never finalized", i, lits);
         }
       },
       Step::Del(i, lits) => {
