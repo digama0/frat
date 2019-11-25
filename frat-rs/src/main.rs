e876e9123dbc70ecae6171ea3dae68aa7a525072
Mario Carneiro <di.gama@gmail.com>
2019-11-24 19:09:51 -0500

    rename lratchk to fratchk

--- frat-rs/src/main.rs
+++ frat-rs/src/main.rs
@@ -2,7 +2,7 @@ mod dimacs;
 mod parser;
 mod backparser;
 mod elab;
-mod lratchk;
+mod fratchk;
 mod dratchk;
 mod serialize;
 
@@ -12,7 +12,7 @@ use std::io;
 fn main() -> io::Result<()> {
   let mut args = env::args().skip(1);
   match args.next().expect("expected a subcommand").as_str() {
-    "fratchk" => lratchk::main(args),
+    "fratchk" => fratchk::main(args),
     "dratchk" => dratchk::main(args),
     "elab" => elab::main(args),
     _ => panic!("incorrect arguments")
