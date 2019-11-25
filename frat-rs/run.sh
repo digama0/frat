5f953a29f1c59fec4ac5d2a29f4cc7d9701623e4
Mario Carneiro <di.gama@gmail.com>
2019-11-25 01:11:41 -0500

    use elab -v instead of lrat-check

--- frat-rs/run.sh
+++ frat-rs/run.sh
@@ -4,9 +4,10 @@
 case $? in
   10) echo "SATISFIABLE";;
   20) # ../drat-trim/drat-trim $1 $2 -C -l $2 > /dev/null
-      cargo run --bin elab --release -- $1 $2 $3 && \
-      ../../drat-trim/lrat-check $1 $3;;
-      # cargo run --bin frat-rs --release -- $2;;
+      # cargo run --release -- elab $1 $2 $3 && \
+      # ../../drat-trim/lrat-check $1 $3;;
+      cargo run --release -- elab $1 $2 $3 -v;;
+      # cargo run --release -- fratchk $2;;
   *)  echo ERROR
       return -1;;
 esac
